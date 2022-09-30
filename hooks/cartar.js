1b433700975e6f8a4201c04ff104beae3bab585e
Marcus Christiansson <christiansson.marcus@gmail.com>
2022-09-30 22:49:41 +0200

    Updated to latest eslint config.

--- hooks/cartar.js
+++ hooks/cartar.js
@@ -3,13 +3,16 @@ import _ from "lodash";
 import { useCallback } from "react";
 
 export default function useCartar(defaultProps) {
-  const get = useCallback((props) => {
-    // React props are in camelCase but query string needs to be in kebab-case.
-    const allProps = { ...defaultProps, ...props };
-    const queryString = _.mapKeys(allProps, (v, k) => _.kebabCase(k));
+  const get = useCallback(
+    (props) => {
+      // React props are in camelCase but query string needs to be in kebab-case.
+      const allProps = { ...defaultProps, ...props };
+      const queryString = _.mapKeys(allProps, (v, k) => _.kebabCase(k));
 
-    return createUrl("https://cartar.io", queryString);
-  }, defaultProps);
+      return createUrl("https://cartar.io", queryString);
+    },
+    [defaultProps]
+  );
 
   return { get };
 }
