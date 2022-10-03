10ba47ebf0b5c97799e49df27fb26440b485a6a5
Marcus Christiansson <christiansson.marcus@gmail.com>
2022-10-03 20:56:41 +0200

    Added prop types.

--- components/Cartar.js
+++ components/Cartar.js
@@ -2,23 +2,49 @@ import PropTypes from "prop-types";
 import { Image } from "react-native";
 
 import useCartar from "../hooks/cartar";
+import Placeholder from "../images/placeholder.png";
 
 export default function Cartar({ size, style, ...rest }) {
   const { get } = useCartar();
 
-  const source = get({ size, ...rest });
+  // Fetch twice as big to get good quality.
+  const uri = get({ size: size * 2, ...rest });
 
   return (
-    <Image source={source} style={[{ height: size, width: size }, style]} />
+    <Image
+      defaultSource={Placeholder}
+      source={{ uri }}
+      style={[{ height: size, width: size }, style]}
+    />
   );
 }
 
 Cartar.propTypes = {
+  beard: PropTypes.number,
+  beardColor: PropTypes.string,
+  eye: PropTypes.number,
+  hair: PropTypes.number,
+  hairColor: PropTypes.string,
+  mouth: PropTypes.number,
+  shirt: PropTypes.number,
+  shirtColor: PropTypes.string,
+  shirtColorAlt: PropTypes.string,
   size: PropTypes.number,
+  skinColor: PropTypes.string,
   style: PropTypes.any,
 };
 
 Cartar.defaultProps = {
+  beard: 0,
+  beardColor: "",
+  eye: 0,
+  hair: 0,
+  hairColor: "",
+  mouth: 0,
+  shirt: 1,
+  shirtColor: "",
+  shirtColorAlt: "",
   size: 400,
+  skinColor: "",
   style: {},
 };
--- hooks/cartar.js
+++ hooks/cartar.js
@@ -5,9 +5,15 @@ import { useCallback } from "react";
 export default function useCartar(defaultProps) {
   const get = useCallback(
     (props) => {
+      let queryString = { ...defaultProps, ...props };
+
       // React props are in camelCase but query string needs to be in kebab-case.
-      const allProps = { ...defaultProps, ...props };
-      const queryString = _.mapKeys(allProps, (v, k) => _.kebabCase(k));
+      queryString = _.mapKeys(queryString, (v, k) => _.kebabCase(k));
+
+      // Remove all "#" from color props.
+      queryString = _.mapValues(queryString, (v) =>
+        v?.toString().replace("#", "")
+      );
 
       return createUrl("https://cartar.io", queryString);
     },
