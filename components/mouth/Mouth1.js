76aa1cd9d7b18edb7fbb7229a05bc581014ae62c
Marcus Christiansson <christiansson.marcus@gmail.com>
2023-12-05 22:06:35 +0100

    Replaced all old mouths.

--- components/mouth/Mouth1.js
+++ components/mouth/Mouth1.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Path } from "react-native-svg";
+import { G, Path } from "react-native-svg";
 
-export default function Mouth1({ mouthColor }) {
+export default function Mouth1({ borderColor2 }) {
   return (
-    <Path
-      d="M0.300413 0.3875c0.00472047,-0.0285394 0.0625197,-0.0283071 0.0651457,0 0,0 -0.0293307,-0.0189409 -0.0651457,0z"
-      fill={mouthColor}
-    />
+    <G scale="0.00001">
+      <Path
+        d="M38616 37069c-3474,-2017 -7389,-1873 -9872,782 -160,171 -457,78 -257,-217 2747,-4176 7338,-4058 10129,-565z"
+        fill={borderColor2}
+      />
+      <Path
+        d="M27402 37321c1216,-377 2296,199 1615,1519 -283,-875 -903,-1345 -1615,-1519z"
+        fill={borderColor2}
+      />
+      <Path
+        d="M39324 36383c-1074,27 -1648,609 -988,1555 75,-649 454,-1344 988,-1555z"
+        fill={borderColor2}
+      />
+    </G>
   );
 }
--- components/mouth/Mouth5.js
+++ components/mouth/Mouth5.js
@@ -1,16 +1,37 @@
+import { changeColor } from "@ckbab/js-utils";
 import React from "react";
-import { Rect } from "react-native-svg";
+import { G, Path } from "react-native-svg";
 
-export default function Mouth5({ mouthColor }) {
+export default function Mouth5({
+  borderColor2,
+  teethColor1,
+  teethColor2,
+  tongueColor,
+}) {
+  const darkTongueColor = changeColor(tongueColor, "#000", 0.8);
+
   return (
-    <Rect
-      fill={mouthColor}
-      height="0.0495909"
-      rx="0.0911732"
-      ry="0.0911732"
-      width="0.0326994"
-      x="0.316637"
-      y="0.351566"
-    />
+    <G scale="0.00001">
+      <Path
+        d="M33658 34060c-2296,-115 -4504,756 -5593,2876 -3428,6677 14152,6694 10472,-8 -994,-1811 -2853,-2766 -4879,-2868z"
+        fill={borderColor2}
+      />
+      <Path
+        d="M30115 39855c1329,1643 5176,1743 6463,44 -691,-890 -1888,-1478 -3249,-1478 -1338,0 -2519,569 -3215,1434z"
+        fill={darkTongueColor}
+      />
+      <Path
+        d="M35612 39044c-641,-392 -1430,-624 -2283,-624 -837,0 -1613,222 -2246,601 -4,32 -7,65 -7,98 0,688 1017,1245 2270,1245 1254,0 2270,-557 2270,-1245 0,-25 -2,-50 -4,-75z"
+        fill={tongueColor}
+      />
+      <Path
+        d="M33612 34872c-1710,-86 -3387,457 -4388,1826 1071,416 2513,671 4100,671 1564,0 2987,-248 4053,-653 -891,-1163 -2278,-1769 -3765,-1844z"
+        fill={teethColor2}
+      />
+      <Path
+        d="M30057 36969c939,253 2061,400 3267,400 1184,0 2288,-142 3217,-387 9,-54 15,-109 15,-165 0,-953 -1457,-1726 -3256,-1726 -1798,0 -3256,772 -3256,1726 0,51 4,101 13,152z"
+        fill={teethColor1}
+      />
+    </G>
   );
 }
