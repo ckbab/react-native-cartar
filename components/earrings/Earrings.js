b008962adf716dfd60b56ce45d0716ad230a9900
Marcus Christiansson <christiansson.marcus@gmail.com>
2024-01-05 05:29:54 +0100

    Added cross and long earrings.

--- components/earrings/Earrings.js
+++ components/earrings/Earrings.js
@@ -4,11 +4,21 @@ import Earrings1 from "./Earrings1";
 import Earrings2 from "./Earrings2";
 import Earrings3 from "./Earrings3";
 import Earrings4 from "./Earrings4";
+import Earrings5 from "./Earrings5";
 import Earrings6 from "./Earrings6";
+import Earrings7 from "./Earrings7";
 
 export default function Earrings({ borderColor2, color, type }) {
   const components = useMemo(
-    () => [Earrings1, Earrings2, Earrings3, Earrings4, null, Earrings6],
+    () => [
+      Earrings1,
+      Earrings2,
+      Earrings3,
+      Earrings4,
+      Earrings5,
+      Earrings6,
+      Earrings7,
+    ],
     [],
   );
 
